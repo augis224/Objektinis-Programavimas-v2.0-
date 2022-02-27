de3a87ad9dca4755f072d5769c8b062b06c8c884
Augustas <100161683+augis224@users.noreply.github.com>
2022-02-28 01:07:17 +0200

    Create bibliotekos.h

---
+++ v.0.1.2_vek/bibliotekos.h
@@ -0,0 +1,17 @@
+#ifndef BIBLIOTEKOS_H_INCLUDED
+#define BIBLIOTEKOS_H_INCLUDED
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+#include <vector>
+
+using std::cout;
+using std::cin;
+using std::string;
+using namespace std;
+
+#endif // BIBLIOTEKOS_H_INCLUDED
