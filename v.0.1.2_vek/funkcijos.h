91d59b1451782cf9bbb77cfac2ca9097362fde41
Augustas <100161683+augis224@users.noreply.github.com>
2022-02-28 01:07:51 +0200

    Create funkcijos.h

---
+++ v.0.1.2_vek/funkcijos.h
@@ -0,0 +1,9 @@
+#ifndef FUNKCIJOS_H_INCLUDED
+#define FUNKCIJOS_H_INCLUDED
+
+void Ivedimas(vector<data>& sarasas);
+void ArIntTikrinimas();
+int AtsitiktinisSkaicius();
+void Isvedimas(vector<data>& sarasas, int i);
+
+#endif // FUNKCIJOS_H_INCLUDED
