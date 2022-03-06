89de7b17f1f08b5589851e63b15e64d7c419ab7c
Augustas <100161683+augis224@users.noreply.github.com>
2022-03-06 22:08:13 +0200

    Delete v.0.1.2_vek directory

--- v.0.1.2_vek/funkcijos.h
+++
@@ -1,9 +0,0 @@
-#ifndef FUNKCIJOS_H_INCLUDED
-#define FUNKCIJOS_H_INCLUDED
-
-void Ivedimas(vector<data>& sarasas);
-void ArIntTikrinimas();
-int AtsitiktinisSkaicius();
-void Isvedimas(vector<data>& sarasas, int i);
-
-#endif // FUNKCIJOS_H_INCLUDED
