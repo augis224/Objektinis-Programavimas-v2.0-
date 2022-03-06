cb5a1ec39be392758ab59da00824500118d62fef
Augustas <100161683+augis224@users.noreply.github.com>
2022-03-06 22:10:00 +0200

    Create funkcijos.h

---
+++ v.01_vek/funkcijos.h
@@ -0,0 +1,10 @@
+#ifndef FUNKCIJOS_H_INCLUDED
+#define FUNKCIJOS_H_INCLUDED
+
+void Ivedimas(data& temp);
+void ArIntTikrinimas();
+int AtsitiktinisSkaicius();
+void IsvedimoParuosimas();
+void Isvedimas(data& temp);
+
+#endif // FUNKCIJOS_H_INCLUDED
