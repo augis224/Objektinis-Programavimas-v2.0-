847e8bb7ec40acc06171c844deffcfd6af29df2b
Augustas <100161683+augis224@users.noreply.github.com>
2022-03-12 21:47:16 +0200

    Create bibliotekos.h

---
+++ v.01_vek/bibliotekos.h
@@ -0,0 +1,28 @@
+#ifndef BIBLIOTEKOS_H_INCLUDED
+#define BIBLIOTEKOS_H_INCLUDED
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+#include <vector>
+
+using std::cout;
+using std::cin;
+using std::string;
+using namespace std;
+
+int StudentuCounter = 0, NdCounter;
+string tnPasirinkimas, paPasirinkimas, VedimoPasirinkimas;
+
+struct data
+{
+    string Vardas, Pavarde;
+    int Nd[25], Egz, NdSk;
+    double GalutinisVid, GalutinisMed;
+};
+
+#endif // BIBLIOTEKOS_H_INCLUDED
