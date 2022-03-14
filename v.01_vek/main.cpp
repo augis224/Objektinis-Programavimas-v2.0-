740a9de88343afdbe2b51c3cdc28b50047487f8f
Augustas <100161683+augis224@users.noreply.github.com>
2022-03-14 08:42:26 +0200

    Update main.cpp

--- v.01_vek/main.cpp
+++ v.01_vek/main.cpp
@@ -5,12 +5,16 @@ int main()
 {
     while(VedimoPasirinkimas != "p" && VedimoPasirinkimas != "f")
     {
-        cout << "Ar norite duomenis ivesti patys, ar nuskaityti is failo? (p - patys, f - is failo): "; cin >> VedimoPasirinkimas;
+        cout << "Ar norite duomenis ivesti patys, ar nuskaityti is failo? (p - patys, f - is failo): ";
+        cin >> VedimoPasirinkimas;
     }
-
     cout << "\n";
+
     vector<data> sarasas;
     data temp;
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+//  Duomenu suvedimas ir isvedimas.
 
     if(VedimoPasirinkimas == "p")
     {
@@ -26,40 +30,83 @@ int main()
             cout << "\n";
         }
 
+        VedimoPasirinkimas = "p";
         IsvedimoParuosimas();
-        for(int i = 0; i < sarasas.size(); i++)
+        for(int i = 0; i < (int)sarasas.size(); i++)
         {
             Isvedimas(sarasas.at(i));
         }
     }
 
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+//  Duomenu ivedimas ir isvedimas is failo.
+
     if(VedimoPasirinkimas == "f")
     {
-        ifstream file("kursiokai.txt");
+        vector<string> Splitted;
+        string Eilute;
+        stringstream Buferis;
 
-        if(!file)
+        ifstream file("studentai1.txt");
+        Buferis << file.rdbuf();
+        file.close();
+
+        while(Buferis)
         {
-            cerr << "Failo 'kursiokai.txt' atidaryti nepavyko." << endl;
-            terminate();
+            if(!Buferis.eof())
+            {
+                getline(Buferis, Eilute);
+                Splitted.push_back(Eilute);
+            }
+            else break;
         }
 
-        for(int i = 0; i < 10; i++)
+        string Outputas = "";
+        for (string &a: Splitted)(a.compare(*Splitted.rbegin()) !=0) ? Outputas += a + "\n" : Outputas += a;
+
+        cout << "Eiluciu skaicius: " << Splitted.size() << "\n";
+        cout << "Vienos eilutes simboliu skaicius: " << Splitted.at(0).size() << "\n";
+
+        //Skaiciuojama kiek zodziu yra vienoje eiluteje.
+        string zodziuSkaicius = Splitted.at(0);
+        size_t NWords = zodziuSkaicius.empty() || zodziuSkaicius.back() == ' ' ? 0 : 1;
+        for(size_t s = zodziuSkaicius.size(); s > 0; --s)
         {
-            file >> temp.Vardas >> temp.Pavarde;
-            for(int i = 0; i < 15; i++)
+            if(zodziuSkaicius[s] == ' ' && zodziuSkaicius[s-1] != ' ')
             {
-                file >> temp.Nd[i];
-                temp.NdSk = 15;
+                ++NWords;
             }
-            file >> temp.Egz;
+        }
+
+        cout << "Elementu skaicius eiluteje: " << NWords << "\n";
+
+        ifstream ofile("studentai1.txt");
+        //Praleidziama pirma eilute.
+        ofile.ignore(numeric_limits<streamsize>::max(), '\n');
+        for(int i = 0; i < Splitted.size() - 1; i++)
+        {
+            ofile >> temp.Vardas >> temp.Pavarde;
+            for(int i = 0; i < (int)NWords - 3; i++)
+            {
+                ofile >> temp.Nd[i];
+            }
+            temp.NdSk = NWords - 3;
+            ofile >> temp.Egz;
             sarasas.push_back(temp);
         }
 
+        //Isvedam apdorotus duomenis i faila.
+        freopen("isvestis.txt", "w", stdout);
         IsvedimoParuosimas();
-        for(int i = 0; i < sarasas.size(); i++)
+        for(int i = 0; i < Splitted.size() - 1; i++)
         {
             Isvedimas(sarasas.at(i));
         }
+        cout << "\n";
+        freopen("CON","w",stdout);
+        cout << "\n" << "Apdoroti duomenys isvesti i faila 'isvestis.txt'." << "\n";
+
+        Splitted.clear();
     }
 }
 
