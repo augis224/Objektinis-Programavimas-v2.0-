b3ca0c9b78bc682ca745133bd947969d1c92e0e3
Augustas <100161683+augis224@users.noreply.github.com>
2022-03-27 16:58:19 +0300

    Update bibliotekos.h

--- v.01_vek/bibliotekos.h
+++ v.01_vek/bibliotekos.h
@@ -2,21 +2,28 @@
 #define BIBLIOTEKOS_H_INCLUDED
 
 #include <iostream>
-#include <fstream>
 #include <iomanip>
-#include <string>
+#include <stdio.h>
 #include <cstdlib>
-#include <ctime>
-#include <algorithm>
+#include <fstream>
+#include <sstream>
 #include <vector>
+#include <string>
+#include <ctime>
+#include <cstring>
+#include <chrono>
+#include <algorithm>
+#include <limits>
+#include <random>
 
 using std::cout;
 using std::cin;
 using std::string;
 using namespace std;
 
-int StudentuCounter = 0, NdCounter;
+int StudentuCounter = 0, NdCounter, n, EilSk = 100;
 string tnPasirinkimas, paPasirinkimas, VedimoPasirinkimas;
+char FailuVardai[5][14] = {"1000.txt", "10000.txt", "100000.txt", "1000000.txt", "10000000.txt"};
 
 struct data
 {
@@ -24,5 +31,10 @@ struct data
     int Nd[25], Egz = 0, NdSk;
     double GalutinisVid, GalutinisMed;
 };
+
+using hrClock = std::chrono::high_resolution_clock;
+typedef std::uniform_int_distribution<int>  int_distribution;
+mt19937 mt(static_cast<long unsigned int>(hrClock::now().time_since_epoch().count()));
+int_distribution dist(1, 10);
 
 #endif // BIBLIOTEKOS_H_INCLUDED
