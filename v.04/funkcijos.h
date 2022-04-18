a4485d7efa6661b199bd838377842e828d1de4a1
Augustas <100161683+augis224@users.noreply.github.com>
2022-04-18 21:46:56 +0300

    Create funkcijos.h

---
+++ v.04/funkcijos.h
@@ -0,0 +1,14 @@
+#ifndef FUNKCIJOS_H_INCLUDED
+#define FUNKCIJOS_H_INCLUDED
+
+void Ivedimas(data& temp);
+void ArIntTikrinimas();
+int AtsitiktinisSkaicius();
+void IsvedimoParuosimas();
+bool compare(const data&, const data&);
+void Isvedimas(data& temp);
+void FailuGeneravimas();
+void KituFailuGeneravimas();
+void DuomenuGeneravimas(int n, int i);
+
+#endif // FUNKCIJOS_H_INCLUDED
